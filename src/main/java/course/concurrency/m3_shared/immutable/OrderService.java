3ec2bdd6a43ab8f643f77eef8c87269f9ce8b709
zneik <afifty50@gmail.com>
2022-11-13 22:14:23 +0300

    task3-13/4

--- src/main/java/course/concurrency/m3_shared/immutable/OrderService.java
+++ src/main/java/course/concurrency/m3_shared/immutable/OrderService.java
@@ -1,43 +1,42 @@
 package course.concurrency.m3_shared.immutable;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
 
 public class OrderService {
 
-    private Map<Long, Order> currentOrders = new HashMap<>();
-    private long nextId = 0L;
+    private Map<Long, Order> currentOrders = new ConcurrentHashMap<>();
+    private AtomicLong nextId = new AtomicLong();
 
     private synchronized long nextId() {
-        return nextId++;
+        return nextId.getAndIncrement();
     }
 
     public synchronized long createOrder(List<Item> items) {
         long id = nextId();
-        Order order = new Order(items);
-        order.setId(id);
+        Order order = Order.create(id, items);
         currentOrders.put(id, order);
         return id;
     }
 
     public synchronized void updatePaymentInfo(long orderId, PaymentInfo paymentInfo) {
-        currentOrders.get(orderId).setPaymentInfo(paymentInfo);
+        currentOrders.compute(orderId, (k, v) -> v.withPaymentInfo(paymentInfo));
         if (currentOrders.get(orderId).checkStatus()) {
             deliver(currentOrders.get(orderId));
         }
     }
 
     public synchronized void setPacked(long orderId) {
-        currentOrders.get(orderId).setPacked(true);
+        currentOrders.compute(orderId, (k, v) -> v.withPacked(true));
         if (currentOrders.get(orderId).checkStatus()) {
             deliver(currentOrders.get(orderId));
         }
     }
 
     private synchronized void deliver(Order order) {
-        /* ... */
-        currentOrders.get(order.getId()).setStatus(Order.Status.DELIVERED);
+        currentOrders.compute(order.getId(), (k, v) -> v.withStatus(Order.Status.DELIVERED));
     }
 
     public synchronized boolean isDelivered(long orderId) {
